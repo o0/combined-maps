a2af9d10d2721c40f1ebcc43d201b13d3d11217a
Igor Alexeenko <igor.alexeenko@jetbrains.com>
2015-04-23 00:48:20 +0300

    Use Static API of Yandex.Maps to show traffic tiles.

--- out/app.js
+++ out/app.js
@@ -1,38 +1,80 @@
-var AppEntryPoint = (function () {
-    function AppEntryPoint() {
-        this.container = null;
-        this.map = null;
-        this.yandexLayer = null;
-        this.trafficEnabled = false;
-        this.container = document.querySelector('.layout');
-        this.map = window.L.map(this.container);
-        this.map.setView([55.45, 37.37], 13);
-        window.L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg').addTo(this.map);
-        var button = document.createElement('button');
-        button.innerHTML = 'Yandex.Traffic';
-        this.container.appendChild(button);
-        button.addEventListener('click', this.onClick_.bind(this));
-    }
-    AppEntryPoint.prototype.setTrafficLayerEnabled = function (enabled) {
-        if (this.trafficEnabled === enabled) {
-            return;
+var GeoApp;
+(function (GeoApp) {
+    var App = (function () {
+        function App() {
+            this.container = null;
+            this.map = null;
+            this.yandexLayer = null;
+            this.trafficEnabled = false;
+            this.container = document.querySelector('.layout');
+            this.map = window.L.map(this.container);
+            this.map.setView([55.75, 37.65], 10);
+            window.L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+                reuseTiles: true
+            }).addTo(this.map);
+            var button = document.createElement('button');
+            button.innerHTML = 'Yandex.Traffic';
+            this.container.appendChild(button);
+            button.addEventListener('click', this.onClick_.bind(this));
         }
-        this.trafficEnabled = enabled;
-        if (enabled) {
-            var timestamp = (new Date()).getTime();
-            this.yandexLayer = window.L.tileLayer('https://jgo.maps.yandex.net/1.1/tiles?l=trf&lang=ru_RU&x={x}&y={y}&z={z}&tm=' + timestamp).addTo(this.map);
+        App.prototype.setTrafficLayerEnabled = function (enabled) {
+            if (this.trafficEnabled === enabled) {
+                return;
+            }
+            this.trafficEnabled = enabled;
+            if (enabled) {
+                this.yandexLayer = window.L.tileLayer.canvas();
+                this.yandexLayer.drawTile = function (canvas, tilePoint, zoom) {
+                    var ctx = canvas.getContext("2d");
+                    var image = new Image();
+                    var latLng = MapUtils.tileToLatLng(tilePoint.x + 0.5, tilePoint.y + 0.5, zoom);
+                    image.src = [
+                        'http://static-maps.yandex.ru/1.x/?l=trf&lang=ru_RU',
+                        '&ll=',
+                        [latLng[1], latLng[0]].join(','),
+                        '&z=',
+                        zoom,
+                        '&size=256,256',
+                        '&tm=',
+                        (new Date).getTime()
+                    ].join('');
+                    image.onload = function (evt) {
+                        ctx.drawImage(image, 0, 0, 256, 256);
+                    };
+                }.bind(this);
+                this.yandexLayer.addTo(this.map);
+            }
+            else {
+                this.map.removeLayer(this.yandexLayer);
+            }
+        };
+        App.prototype.onClick_ = function (evt) {
+            evt.preventDefault();
+            this.setTrafficLayerEnabled(!this.trafficEnabled);
+        };
+        App.init = function () {
+            return new App;
+        };
+        return App;
+    })();
+    GeoApp.App = App;
+    var MapUtils = (function () {
+        function MapUtils() {
         }
-        else {
-            this.map.removeLayer(this.yandexLayer);
-        }
-    };
-    AppEntryPoint.prototype.onClick_ = function (evt) {
-        evt.preventDefault();
-        this.setTrafficLayerEnabled(!this.trafficEnabled);
-    };
-    AppEntryPoint.init = function () {
-        return new AppEntryPoint();
-    };
-    return AppEntryPoint;
-})();
+        MapUtils.latLngToTile = function (lat, lng, zoom) {
+            return [
+                (Math.ceil((lng + 180) / 360 * Math.pow(2, zoom))),
+                (Math.ceil((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)))
+            ];
+        };
+        MapUtils.tileToLatLng = function (x, y, zoom) {
+            var n = Math.PI - 2 * Math.PI * y / Math.pow(2, zoom);
+            return [
+                (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)))),
+                (x / Math.pow(2, zoom) * 360 - 180)
+            ];
+        };
+        return MapUtils;
+    })();
+})(GeoApp || (GeoApp = {}));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,88 @@
-class AppEntryPoint {
-  private container: Element = null;
-  private map = null;
-  private yandexLayer = null;
-  private trafficEnabled:boolean = false;
+module GeoApp {
+  export class App {
+    private container: Element = null;
+    private map = null;
+    private yandexLayer = null;
+    private trafficEnabled:boolean = false;
 
-  constructor() {
-    this.container = document.querySelector('.layout');
-    this.map = window.L.map(this.container);
-    this.map.setView([55.45, 37.37], 13);
+    constructor() {
+      this.container = document.querySelector('.layout');
+      this.map = window.L.map(this.container);
+      this.map.setView([55.75, 37.65], 10);
 
-    window.L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg').addTo(this.map);
+      window.L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
+        reuseTiles: true
+      }).addTo(this.map);
 
-    var button: HTMLButtonElement = document.createElement('button');
-    button.innerHTML = 'Yandex.Traffic';
-    this.container.appendChild(button);
+      var button: HTMLButtonElement = document.createElement('button');
+      button.innerHTML = 'Yandex.Traffic';
+      this.container.appendChild(button);
 
-    button.addEventListener('click', this.onClick_.bind(this));
-  }
-
-  public setTrafficLayerEnabled(enabled: boolean) {
-    if (this.trafficEnabled === enabled) {
-      return;
+      button.addEventListener('click', this.onClick_.bind(this));
     }
 
-    this.trafficEnabled = enabled;
+    private setTrafficLayerEnabled(enabled: boolean) {
+      if (this.trafficEnabled === enabled) {
+        return;
+      }
 
-    if (enabled) {
-      var timestamp: number = (new Date()).getTime();
-      this.yandexLayer = window.L.tileLayer('https://jgo.maps.yandex.net/1.1/tiles?l=trf&lang=ru_RU&x={x}&y={y}&z={z}&tm=' + timestamp).addTo(this.map);
-    } else {
-      this.map.removeLayer(this.yandexLayer);
+      this.trafficEnabled = enabled;
+
+      if (enabled) {
+        this.yandexLayer = window.L.tileLayer.canvas();
+        this.yandexLayer.drawTile = function(canvas, tilePoint, zoom) {
+          var ctx = canvas.getContext("2d");
+          var image = new Image();
+          var latLng = MapUtils.tileToLatLng(tilePoint.x + 0.5, tilePoint.y + 0.5, zoom);
+
+          image.src = [
+            // fixme: use this overlay url instead of Static API.
+            //'https://jgo.maps.yandex.net/1.1/tiles?l=trf,trfe&s=&lang=ru_RU',
+            //'&x=', tilePoint.x,
+            //'&y=', tilePoint.y,
+
+            'http://static-maps.yandex.ru/1.x/?l=trf&lang=ru_RU',
+            '&ll=', [latLng[1], latLng[0]].join(','),
+            '&z=', zoom,
+            '&size=256,256',
+            '&tm=', (new Date).getTime()
+          ].join('');
+
+          image.onload = function(evt) {
+            ctx.drawImage(image, 0, 0, 256, 256);
+          }
+        }.bind(this);
+
+        this.yandexLayer.addTo(this.map);
+      } else {
+        this.map.removeLayer(this.yandexLayer);
+      }
+    }
+
+    private onClick_(evt: MouseEvent) {
+      evt.preventDefault();
+      this.setTrafficLayerEnabled(!this.trafficEnabled);
+    }
+
+    public static init(): App {
+      return new App;
     }
   }
 
-  private onClick_(evt: MouseEvent) {
-    evt.preventDefault();
-    this.setTrafficLayerEnabled(!this.trafficEnabled);
-  }
+  class MapUtils {
+    static latLngToTile(lat:number, lng: number, zoom:number):Array<number> {
+      return [
+        (Math.ceil((lng + 180) / 360 * Math.pow(2,zoom))),
+        (Math.ceil((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)))
+      ];
+    }
 
-  public static init() {
-    return new AppEntryPoint();
+    static tileToLatLng(x:number, y:number, zoom:number):Array<number> {
+      var n = Math.PI - 2 * Math.PI * y / Math.pow(2, zoom);
+      return [
+        (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)))),
+        (x / Math.pow(2, zoom) * 360-180)
+      ];
+    }
   }
 }
